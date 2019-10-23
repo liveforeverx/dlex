65d2f34e73d0991409359821e4c7273322108413
Eric Hagman <emhagman@gmail.com>
2019-10-23 12:15:29 -0400

    Add script to start standalone dgraph with docker (#37)

--- test/dlex_test.exs
+++ test/dlex_test.exs
@@ -2,9 +2,9 @@ defmodule DlexTest do
   use ExUnit.Case
 
   setup_all do
-    # {:ok, pid} = Dlex.start_link(pool_size: 2, port: 8080, transport: :http)
-    {:ok, pid} = Dlex.start_link(pool_size: 2)
 
+    # Setup our server
+    {:ok, pid} = Dlex.start_link(pool_size: 2, port: 9090)
     Dlex.alter!(pid, %{drop_all: true})
 
     schema = """
