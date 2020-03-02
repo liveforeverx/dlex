f625aff15b0b6147fdfc05106495b2ba80811efe
Dmitry Russ <dmitry.a.russ@gmail.com>
2020-03-02 11:41:05 +0100

    Update to dgraph v2.0.0-rc1 and dependencies

--- start-server.sh
+++ start-server.sh
@@ -6,6 +6,6 @@ if docker ps -a --format '{{.Names}}' | grep -Eq "^${DGRAPH_CONTAINER_NAME}\$"; 
   echo "Already running..."
 else
   echo "Starting local dgraph server via Docker..."
-  docker run --name $DGRAPH_CONTAINER_NAME -p 8090:8080 -p 9090:9080 -d dgraph/standalone:v1.1.1
+  docker run --name $DGRAPH_CONTAINER_NAME -p 8090:8080 -p 9090:9080 -d dgraph/standalone:v2.0.0-rc1
 fi
 echo "Done."
